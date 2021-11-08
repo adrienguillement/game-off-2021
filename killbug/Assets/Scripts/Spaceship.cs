07fb543aabb91ae255af1a4c5eefc07dcbae3529
Adrien Guillement <adrien.guillement@gmail.com>
2021-11-08 21:23:09 +0100

    Change Firerate and input

--- killbug/Assets/Scripts/Spaceship.cs
+++ killbug/Assets/Scripts/Spaceship.cs
@@ -34,7 +34,7 @@ public class Spaceship : MonoBehaviour
 
         rb.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
 
-        if (Input.GetButton("Fire1") && delay > firerate)
+        if (Input.GetButton("Fire2") && delay > firerate)
         {
             Shoot();
         }
