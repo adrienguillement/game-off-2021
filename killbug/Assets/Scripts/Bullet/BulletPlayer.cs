bf2400664b7ebdd1f051b08d98cbb6b2ae62aa0f
KurokuZa <alexis.gauvrit.85@gmail.com>
2021-11-18 22:38:19 +0100

    Add Player Bullet sound

--- killbug/Assets/Scripts/Bullet/BulletPlayer.cs
+++ killbug/Assets/Scripts/Bullet/BulletPlayer.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class BulletPlayer : MonoBehaviour
 {
     public int bulletSpeed = 10;
+    public AudioClip clip;
+
+    public AudioSource sound;
+    
 
     public Rigidbody2D rb;
     private Vector2 directionShoot;
+    //private AudioSource source {  get { return GetComponent<AudioSource>(); } }
 
     void Awake()
     {
@@ -18,13 +23,25 @@ public class BulletPlayer : MonoBehaviour
         rb.velocity = directionShoot * bulletSpeed;
     }
 
+    void Start()
+    {
+        // Son fonctionnel 1
+        //AudioSource.PlayClipAtPoint(clip, transform.position, 1f);
+
+
+
+        
+        //sound.PlayOneShot(clip);
+
+        sound.Play();
+    }
+
     void Update()
     {
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-
         // If bullet touch wall
         if (col.gameObject.tag == "bounds")
         {
