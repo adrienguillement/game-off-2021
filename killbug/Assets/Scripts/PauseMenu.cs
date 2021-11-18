0213197b5a9d27f3328e8df103d23259aabfa32b
Adrien Guillement <adrien.guillement@gmail.com>
2021-11-18 22:10:45 +0100

    Add countdowntimer on resume pause

--- killbug/Assets/Scripts/PauseMenu.cs
+++ killbug/Assets/Scripts/PauseMenu.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class PauseMenu : MonoBehaviour
 {
+    public Text CountDownText;
     public static bool isPaused = false;
     public GameObject pauseMenuUI;
+    public int countDownOnResume = 3;
+
+    void Awake()
+    {
+
+        CountDownText.enabled = false;
+    }
 
     void Update()
     {
@@ -23,17 +32,18 @@ public class PauseMenu : MonoBehaviour
     IEnumerator Countdown(int seconds)
     {
 
-        Debug.Log("jgjr");
         int counter = seconds;
-        while (counter > 0)
+        while (counter > 1)
         {
             yield return new WaitForSecondsRealtime(1);
             counter--;
-            Debug.Log(counter.ToString());
+            CountDownText.text = counter.ToString();
         }
 
         yield return new WaitForSecondsRealtime(1);
 
+        CountDownText.enabled = false;
+        CountDownText.text = countDownOnResume.ToString();
 
         Time.timeScale = 1f;
         isPaused = false;
@@ -42,7 +52,9 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
-        StartCoroutine(Countdown(3));
+
+        CountDownText.enabled = true;
+        StartCoroutine(Countdown(countDownOnResume));
     }
 
     void Pause()
