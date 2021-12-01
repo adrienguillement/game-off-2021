702f6b3de59d563bf6ecd9a5bc2aebd8e628dea4
KurokuZa <alexis.gauvrit.85@gmail.com>
2021-12-01 18:04:44 +0100

    go to rever

--- killbug/Assets/Scripts/SoundManager.cs
+++ killbug/Assets/Scripts/SoundManager.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public AudioClip musicClip;
-    public float musicVolume;
     private static AudioSource audioSource;
 
     void Awake()
     {
         audioSource = GetComponent<AudioSource>();
-        audioSource.PlayOneShot(musicClip, musicVolume);
     }
 
     public static void PlayClip(AudioClip clip, float volume)
